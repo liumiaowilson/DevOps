eb6858ad430d6fc4a924a021b0d01aa0616e443a
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-02-12 11:19:34 +0000

    add previewLwc

---
+++ cb_js/previewLwc.js
@@ -0,0 +1,25 @@
+(function(cmd, context) {
+    const compName = context.argv[0];
+    if(!compName) {
+        cmd.error('Component Name(c:cmp) is required');
+        return;
+    }
+
+    const attrs = {};
+    context.argv.slice(1).forEach(arg => {
+        const [ key, value ] = arg.split('=');
+        attrs[key] = value;
+    });
+
+    const compDefinition = {
+        componentDef: compName,
+        attributes: attrs,
+    };
+    const encodedCompDef = Buffer.from(JSON.stringify(compDefinition)).toString('base64');
+
+    const url = context.connection.instanceUrl + '/one/one.app#' + encodedCompDef;
+
+    cmd.log(url);
+    context.open(url);
+    return url;
+})

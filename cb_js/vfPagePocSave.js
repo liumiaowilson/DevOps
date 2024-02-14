cf2bf41107fea2f934d4682373aa29635ace29d4
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-02-14 01:15:43 +0000

    add vfPagePocSave

---
+++ cb_js/vfPagePocSave.js
@@ -0,0 +1,50 @@
+(function(cmd, context) {
+    const pageName = 'TestVfPagePOC4735';
+    const controllerName = pageName + 'Controller';
+    const fileName = pageName[0].toLowerCase() + pageName.substring(1);
+    const homeDir = '/home/codebuilder/';
+    const projectName = 'VfPagePoc';
+
+    return context.fs.readFile(homeDir + projectName + '/setting.json', 'utf8').then(content => {
+        const setting = JSON.parse(content);
+
+        return Promise.all([
+            context.fs.readFile(homeDir + projectName + '/pages/' + pageName + '.page', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/pages/' + pageName + '.page-meta.xml', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/classes/' + controllerName + '.cls', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/classes/' + controllerName + '.cls-meta.xml', 'utf8'),
+        ]).then(([ page, pageMeta, controller, controllerMeta, ]) => {
+            const poc = {
+                page,
+                pageMeta,
+                controller,
+                controllerMeta,
+            };
+
+            if(setting.recordId) {
+                return context.mypim.sobject('Item__c').update({
+                    Id: setting.recordId,
+                    Answer__c: JSON.stringify(poc),
+                });
+            }
+            else {
+                return context.mypim.query(`SELECT Id FROM Item__c WHERE Type__c = 'PlainTextCategory' AND Name = 'VfPagePOC'`).then(data => {
+                    return context.inquirer.prompt({
+                        type: 'input',
+                        name: 'name',
+                        message: 'Name of Poc:',
+                    }).then(resp => {
+                        return context.mypim.sobject('Item__c').create({
+                            Name: resp.name,
+                            Type__c: 'PlainText',
+                            Parent__c: data.records[0].Id,
+                            Answer__c: JSON.stringify(poc),
+                        });
+                    });
+                });
+            }
+        }).then(() => {
+            cmd.logSuccess('Done');
+        });
+    });
+})

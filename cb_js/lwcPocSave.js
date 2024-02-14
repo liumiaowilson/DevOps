692a5c0f296ee67227a277955f6a9c5b01dee31d
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-02-14 00:28:03 +0000

    add lwcPocSave

---
+++ cb_js/lwcPocSave.js
@@ -0,0 +1,51 @@
+(function(cmd, context) {
+    const lwcName = 'TestLwcPOC4921';
+    const fileName = lwcName[0].toLowerCase() + lwcName.substring(1);
+    const homeDir = '/home/codebuilder/';
+    const projectName = 'LwcPoc';
+
+    return context.fs.readFile(homeDir + projectName + '/setting.json', 'utf8').then(content => {
+        const setting = JSON.parse(content);
+
+        return Promise.all([
+            context.fs.readFile(homeDir + projectName + '/lwc/' + fileName + '/' + fileName + '.js', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/lwc/' + fileName + '/' + fileName + '.js-meta.xml', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/lwc/' + fileName + '/' + fileName + '.html', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/lwc/' + fileName + '/' + fileName + '.css', 'utf8'),
+            context.fs.readFile(homeDir + projectName + '/payload.json', 'utf8'),
+        ]).then(([ lwcJs, lwcMeta, lwcHtml, lwcCss, lwcPayload, ]) => {
+            const poc = {
+                js: lwcJs,
+                meta: lwcMeta,
+                html: lwcHtml,
+                css: lwcCss,
+                payload: lwcPayload,
+            };
+
+            if(setting.recordId) {
+                return context.mypim.sobject('Item__c').update({
+                    Id: setting.recordId,
+                    Answer__c: JSON.stringify(poc),
+                });
+            }
+            else {
+                return context.mypim.query(`SELECT Id FROM Item__c WHERE Type__c = 'PlainTextCategory' AND Name = 'LwcPOC'`).then(data => {
+                    return context.inquirer.prompt({
+                        type: 'input',
+                        name: 'name',
+                        message: 'Name of Poc:',
+                    }).then(resp => {
+                        return context.mypim.sobject('Item__c').create({
+                            Name: resp.name,
+                            Type__c: 'PlainText',
+                            Parent__c: data.records[0].Id,
+                            Answer__c: JSON.stringify(poc),
+                        });
+                    });
+                });
+            }
+        }).then(() => {
+            cmd.logSuccess('Done');
+        });
+    });
+})

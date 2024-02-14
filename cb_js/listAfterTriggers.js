7fd2466a870aa20257738e3d63d735c938973523
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-02-14 12:05:44 +0000

    add listBeforeTriggers and listAfterTriggers

---
+++ cb_js/listAfterTriggers.js
@@ -0,0 +1,45 @@
+(function(cmd, context) {
+    const objectApiName = context.argv[0];
+    if(!objectApiName) {
+        cmd.error('Object Api Name is required');
+        return;
+    }
+
+    return context.connection.query(`SELECT Id, Name, NamespacePrefix, UsageAfterInsert, UsageAfterUpdate, UsageAfterDelete, UsageAfterUndelete FROM ApexTrigger WHERE TableEnumOrId = '${objectApiName}' AND Status = 'Active' AND (UsageAfterInsert = true OR UsageAfterUpdate = true OR UsageAfterDelete = true OR UsageAfterUndelete = true) ORDER BY Name ASC`).then(data => {
+        const triggers = data.records.map(r => {
+            const name = r.NamespacePrefix ? r.NamespacePrefix + '__' + r.Name : r.Name;
+            const usage = [];
+            if(r.UsageAfterInsert) {
+                usage.push('insert');
+            }
+            if(r.UsageAfterUpdate) {
+                usage.push('update');
+            }
+            if(r.UsageAfterDelete) {
+                usage.push('delete');
+            }
+            if(r.UsageAfterUndelete) {
+                usage.push('undelete');
+            }
+
+            const trigger = {
+                id: r.Id,
+                name,
+                usage: usage.join(','),
+            };
+
+            return trigger;
+        });
+
+        if(triggers.length) {
+            cmd.styledHeader('# After Triggers');
+            context.ux.table(triggers, {
+                id: {},
+                name: {},
+                usage: {},
+            });
+        }
+
+        return triggers;
+    });
+})
---
+++ cb_js/listBeforeTriggers.js
@@ -0,0 +1,42 @@
+(function(cmd, context) {
+    const objectApiName = context.argv[0];
+    if(!objectApiName) {
+        cmd.error('Object Api Name is required');
+        return;
+    }
+
+    return context.connection.query(`SELECT Id, Name, NamespacePrefix, UsageBeforeInsert, UsageBeforeUpdate, UsageBeforeDelete FROM ApexTrigger WHERE TableEnumOrId = '${objectApiName}' AND Status = 'Active' AND (UsageBeforeInsert = true OR UsageBeforeUpdate = true OR UsageBeforeDelete = true) ORDER BY Name ASC`).then(data => {
+        const triggers = data.records.map(r => {
+            const name = r.NamespacePrefix ? r.NamespacePrefix + '__' + r.Name : r.Name;
+            const usage = [];
+            if(r.UsageBeforeInsert) {
+                usage.push('insert');
+            }
+            if(r.UsageBeforeUpdate) {
+                usage.push('update');
+            }
+            if(r.UsageBeforeDelete) {
+                usage.push('delete');
+            }
+
+            const trigger = {
+                id: r.Id,
+                name,
+                usage: usage.join(','),
+            };
+
+            return trigger;
+        });
+
+        if(triggers.length) {
+            cmd.styledHeader('# Before Triggers');
+            context.ux.table(triggers, {
+                id: {},
+                name: {},
+                usage: {},
+            });
+        }
+
+        return triggers;
+    });
+})

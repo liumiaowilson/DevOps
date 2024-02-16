2946e44c18c2b9a15388f24ffef507fb6e3b0f53
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-02-16 21:25:45 +0000

    add listAsyncActions

---
+++ cb_js/listAsyncActions.js
@@ -0,0 +1,42 @@
+(function(cmd, context) {
+    const objectApiName = context.argv[0];
+    if(!objectApiName) {
+        cmd.error('Object Api Name is required');
+        return;
+    }
+
+    context.ux.action.start('Calculating Async Actions');
+    return context.connection.query(`SELECT DurableId FROM EntityDefinition WHERE QualifiedApiName = '${objectApiName}'`).then(data => {
+        const record = data.records[0];
+        if(!record) {
+            cmd.error('Invalid object api name: ' + objectApiName);
+            return;
+        }
+
+        const durableId = record.DurableId;
+        return context.connection.query(`SELECT DurableId, ApiName, NamespacePrefix FROM FlowDefinitionView WHERE IsActive = true AND TriggerObjectOrEventId = '${durableId}' AND HasAsyncAfterCommitPath = true ORDER BY TriggerOrder ASC`).then(data => {
+            const flows = data.records;
+
+            if(!flows.length) {
+                return [];
+            }
+
+            const result = flows.map(flow => {
+                return {
+                    id: flow.DurableId,
+                    name: flow.NamespacePrefix ? flow.NamespacePrefix + '__' + flow.ApiName : flow.ApiName,
+                    url: '[m://flow/' + flow.DurableId + ']',
+                };
+            });
+
+            cmd.styledHeader('# Async Actions');
+            context.ux.table(result, {
+                name: {},
+                usage: {},
+                url: {},
+            });
+
+            return result;
+        });
+    }).finally(() => context.ux.action.stop());
+})

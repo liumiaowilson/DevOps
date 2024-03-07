72e755a37c899a86b9bbafdb79b2cb35955461c9
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-03-07 09:27:34 +0000

    add recordTree

---
+++ cb_js/recordTree.js
@@ -0,0 +1,66 @@
+(function(cmd, context) {
+    const recordId = context.argv[0];
+    if(!recordId) {
+        cmd.error('Record Id is required');
+        return;
+    }
+
+    const getRecordName = record => {
+        for(const key of Object.keys(record)) {
+            if(key === 'Id') continue;
+
+            const value = record[key];
+            if(value && typeof value === 'string') {
+                return value;
+            }
+        }
+
+        return record.Id;
+    };
+
+    const buildRecordTree = (record, context, chalk) => {
+        const tree = context.ux.tree();
+
+        Object.keys(record).forEach(key => {
+            const value = record[key];
+            if(value && value.records) {
+                for(const childRecord of value.records) {
+                    const childTree = buildRecordTree(childRecord, context, chalk);
+                    const name = chalk.dim('(' + key + ')') + chalk.green(getRecordName(childRecord)) + '[m://' + childRecord.Id + ']';
+                    tree.insert(name, childTree);
+                }
+            }
+        });
+
+        return tree;
+    };
+
+    context.ux.action.start('Querying');
+    return context.require('chalk').then(({ default: chalk }) => {
+        return context.connection.describeGlobal().then(data => {
+            const prefixMap = {};
+            for(const sobject of data.sobjects) {
+                prefixMap[sobject.keyPrefix] = sobject.name;
+            }
+
+            const objectApiName = prefixMap[recordId.substring(0, 3)];
+            const queryFile = '/home/codebuilder/DevOps/soql/' + objectApiName + '.soql';
+
+            return context.fs.readFile(queryFile, 'utf8').then(query => {
+                query = query.replaceAll('{{recordId}}', recordId);
+
+                return context.connection.query(query).then(data => {
+                    const root = context.ux.tree();
+                    for(const record of data.records) {
+                        const tree = buildRecordTree(record, context, chalk);
+                        const name = chalk.green(getRecordName(record)) + '[m://' + record.Id + ']';
+                        root.insert(name, tree);
+                    }
+
+                    root.display();
+                    return data;
+                });
+            });
+        });
+    }).finally(() => context.ux.action.stop());
+})

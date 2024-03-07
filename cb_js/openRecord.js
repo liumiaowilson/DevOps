1443b9516cc06b779154a58f78daa86a7b4ebca6
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-03-07 11:38:51 +0000

    add id script

---
+++ cb_js/openRecord.js
@@ -0,0 +1,35 @@
+(function(cmd, context) {
+    const recordId = context.argv[0];
+    if(!recordId) {
+        cmd.error('Record Id is required');
+        return;
+    }
+
+    return Promise.all([
+        context.fs.readFile('/home/codebuilder/keyPrefix.json', 'utf8'),
+        context.fs.readFile('/home/codebuilder/apps.json', 'utf8'),
+        context.fs.readFile('/home/codebuilder/DevOps/json/apps.json', 'utf8'),
+    ]).then(([ keyPrefixJSON, appsJSON, userAppsJSON ]) => {
+        const keyPrefixMap = JSON.parse(keyPrefixJSON);
+        const appsMap = JSON.parse(appsJSON);
+        const userAppsMap = JSON.parse(userAppsJSON);
+
+        const keyPrefix = recordId.substring(0, 3);
+        if(keyPrefixMap[keyPrefix]) {
+            const objectApiName = keyPrefixMap[keyPrefix];
+            const appName = userAppsMap[objectApiName];
+            if(appName) {
+                const appId = appsMap[appName];
+                if(appId) {
+                    const url = context.connection.instanceUrl + '/lightning/app/' + appId + '/r/' + objectApiName + '/' + recordId + '/view';
+                    context.open(url);
+                    return url;
+                }
+            }
+        }
+
+        const url = context.connection.instanceUrl + '/' + recordId;
+        context.open(url);
+        return url;
+    });
+})

c5db36630a7a8a9a122545cb6686284e7096ef5b
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-03-13 04:09:48 +0000

    add more recordTree rule

--- js/recordTree.js
+++ js/recordTree.js
@@ -1,5 +1,41 @@
 ({
     practifi__Active_Form_Field__c: function(record) {
-        return record.Name + (record.practifi__Type__c ? `[${record.practifi__Type__c}]` : '');
+        const tags = [];
+        if(record.practifi__Required__c) {
+            tags.push('*');
+        }
+        if(record.practifi__Prefill_This_Field__c) {
+            tags.push('_');
+        }
+        if((record.practifi__Display_To__c && record.practifi__Display_To__c !== 'Internal and Portal Users') ||
+            record.practifi__Visibility_Rule__c ||
+            record.practifi__Display_This_Field_If__c) {
+            tags.push('?');
+        }
+
+        return record.Name +
+            (record.practifi__Type__c ? `[${record.practifi__Type__c}]` : '') +
+            (tags.length ? `[${tags.join('')}]` : '');
+    },
+
+    practifi__Active_Form_Field_Section__c: function(record) {
+        const tags = [];
+        if(record.practifi__Repeat_Using__c) {
+            tags.push('#');
+        }
+        if(record.practifi__Display_This_Field_Section_If__c || record.practifi__Visibility_Rule__c) {
+            tags.push('?');
+        }
+
+        return record.Name + (tags.length ? `[${tags.join('')}]` : '');
+    },
+
+    practifi__Action__c: function(record) {
+        const tags = [];
+        if(record.practifi__Execution_Rule__c) {
+            tags.push('?');
+        }
+
+        return record.Name + (tags.length ? `[${tags.join('')}]` : '');
     },
 })

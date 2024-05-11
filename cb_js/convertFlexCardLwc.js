93a22f77107e8675f4ce5285c8503b28ca954582
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-05-11 01:27:47 +0000

    update convertFlexCardLwc

--- cb_js/convertFlexCardLwc.js
+++ cb_js/convertFlexCardLwc.js
@@ -24,6 +24,7 @@
         context.fs.readFile(jsHtmlPath, 'utf8').then(html => {
             html = html.replace(/omnistudio-/g, 'c-v-');
             html = html.replace(/c-cf-/g, 'c-');
+            html = html.replace(/Parent\./g, '');
             return context.fs.writeFile(jsHtmlPath, html);
         }),
         context.fs.readFile(jsPath, 'utf8').then(js => {

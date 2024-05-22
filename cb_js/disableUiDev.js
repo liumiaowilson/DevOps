db53162fe5d30dc4305fd5e4a71251da7f6acb1e
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-05-22 22:47:16 +0000

    update disabeUiDev and enableUiDev

--- cb_js/disableUiDev.js
+++ cb_js/disableUiDev.js
@@ -1,17 +1,31 @@
 (function(cmd, context) {
-    return context.connection.query(`SELECT Id FROM User WHERE Username = '${context.username}'`).then(data => {
-        const user = data.records[0];
-        return context.connection.metadata.update('MobileSettings', [
-            {
-                fullName: 'Mobile',
-                enableS1EncryptedStoragePref2: true,
+    return context.autocomplete({
+        message: 'Which user do you want to disable ui dev?',
+        source: input => {
+            let query = 'SELECT Id, Username, Name FROM User WHERE IsActive = true';
+            if(input) {
+                query += ` AND (Username LIKE '%${input}%' OR Name LIKE '%${input}%')`
             }
-        ]).then(() => {
-            return context.connection.sobject('User').update({
-                Id: user.Id,
-                UserPreferencesUserDebugModePref: false,
-            }).then(() => {
-                cmd.logSuccess('Done');
+            query += ' ORDER BY Username ASC LIMIT 20';
+            return context.connection.query(query).then(data => {
+                return data.records.map(r => ({ value: r.Username, description: r.Name }));
+            });
+        },
+    }).then(username => {
+        return context.connection.query(`SELECT Id FROM User WHERE Username = '${username}'`).then(data => {
+            const user = data.records[0];
+            return context.connection.metadata.update('MobileSettings', [
+                {
+                    fullName: 'Mobile',
+                    enableS1EncryptedStoragePref2: true,
+                }
+            ]).then(() => {
+                return context.connection.sobject('User').update({
+                    Id: user.Id,
+                    UserPreferencesUserDebugModePref: false,
+                }).then(() => {
+                    cmd.logSuccess('Done');
+                });
             });
         });
     });
--- cb_js/enableUiDev.js
+++ cb_js/enableUiDev.js
@@ -1,17 +1,31 @@
 (function(cmd, context) {
-    return context.connection.query(`SELECT Id FROM User WHERE Username = '${context.username}'`).then(data => {
-        const user = data.records[0];
-        return context.connection.metadata.update('MobileSettings', [
-            {
-                fullName: 'Mobile',
-                enableS1EncryptedStoragePref2: false,
+    return context.autocomplete({
+        message: 'Which user do you want to enable ui dev?',
+        source: input => {
+            let query = 'SELECT Id, Username, Name FROM User WHERE IsActive = true';
+            if(input) {
+                query += ` AND (Username LIKE '%${input}%' OR Name LIKE '%${input}%')`
             }
-        ]).then(() => {
-            return context.connection.sobject('User').update({
-                Id: user.Id,
-                UserPreferencesUserDebugModePref: true,
-            }).then(() => {
-                cmd.logSuccess('Done');
+            query += ' ORDER BY Username ASC LIMIT 20';
+            return context.connection.query(query).then(data => {
+                return data.records.map(r => ({ value: r.Username, description: r.Name }));
+            });
+        },
+    }).then(username => {
+        return context.connection.query(`SELECT Id FROM User WHERE Username = '${username}'`).then(data => {
+            const user = data.records[0];
+            return context.connection.metadata.update('MobileSettings', [
+                {
+                    fullName: 'Mobile',
+                    enableS1EncryptedStoragePref2: false,
+                }
+            ]).then(() => {
+                return context.connection.sobject('User').update({
+                    Id: user.Id,
+                    UserPreferencesUserDebugModePref: true,
+                }).then(() => {
+                    cmd.logSuccess('Done');
+                });
             });
         });
     });

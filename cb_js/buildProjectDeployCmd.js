7d4cf030faa818723cad8ba82ec49fcbe2cce137
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-07-17 04:14:48 +0000

    refactor deployFile and deployGitStatus

---
+++ cb_js/buildProjectDeployCmd.js
@@ -0,0 +1,11 @@
+(function(cmd, context) {
+    const filePaths = context.argv[0];
+    if(!filePaths) {
+        cmd.error('Input files are required');
+        return;
+    }
+
+    const files = filePaths.split(',');
+    const cmdStr = 'sf project deploy start -o $DefaultAlias --ignore-conflicts' + files.map(f => ' -d "' + f + '"').join('');
+    cmd.log(cmdStr);
+})

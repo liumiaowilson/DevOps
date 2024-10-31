863d146de7ae83f6ebb5d9ea5479e6b7bfec4f4f
Wilson Liu <lockleeliumiaowilson@gmail.com>
2024-10-31 09:30:11 +0000

    update querySave

--- cb_js/querySave.js
+++ cb_js/querySave.js
@@ -8,15 +8,33 @@
     const homeDir = context.env.getString('CODE_BUILDER_HOME');
     context.ux.action.start('Saving query');
     return Promise.all([
-        context.fs.readFile(homeDir + '/.saved_queries', 'utf8'),
+        context.mypim.query(`SELECT Id, Answer__c FROM Item__c WHERE Type__c = 'File' AND Name = 'CodeBuilderQueries'`),
         context.fs.readFile(homeDir + '/.last_query', 'utf8'),
     ]).then(([ savedQueries, lastQuery ]) => {
         lastQuery = lastQuery.trim();
-        const queries = JSON.parse(savedQueries || '{}');
+        let record = savedQueries.records[0];
+        const queries = JSON.parse(record?.Answer__c || '{}');
         if(lastQuery) {
             queries[name] = lastQuery;
         }
 
-        return context.fs.writeFile(homeDir + '/.saved_queries', JSON.stringify(queries, null, 4));
+        if(!record) {
+            record = {
+                Type__c: 'File',
+                Name: 'CodeBuilderQueries',
+                Source__c: 'CodeBuilderQueries',
+                Answer__c: JSON.stringify(queries, null, 4),
+            };
+
+            return context.mypim.create('Item__c', record).then(result => result.id);
+        }
+        else {
+            record.Answer__c = JSON.stringify(queries, null, 4);
+
+            return context.mypim.update('Item__c', record).then(result => result.id);
+        }
+    }).then(recordId => {
+        const url = `${context.mypim.instanceUrl}/lightning/cmp/c__itemEditor?c__type=File&c__recordId=${recordId}`;
+        cmd.log(`Query saved. You can edit it [here](${url})`);
     }).finally(() => context.ux.action.stop());
 })
